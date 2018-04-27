c522a4e8b700da2339365ae5b094e0af4369c37c
Dr Alan R.T. Spencer <a.r.t.spencer@gmail.com>
2018-04-27 11:51:11 +0100

    Updated the darkstyle theme icons

--- main.cpp
+++ main.cpp
@@ -22,11 +22,8 @@
 int main(int argc, char *argv[])
 {
     //This has the app draw at HiDPI scaling on HiDPI displays, usually two pixels for every one logical pixel
-    //doesn't work in v5.5
-    #if (QT_VERSION >= 0x050600)
-        QApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
-    #endif
-    qDebug()<<QT_VERSION;
+    QApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+
     //This has QPixmap images use the @2x images when available
     //See this bug for more details on how to get this right: https://bugreports.qt.io/browse/QTBUG-44486#comment-327410
     QApplication::setAttribute(Qt::AA_UseHighDpiPixmaps);
---
+++ version.h
@@ -0,0 +1,44 @@
+/**
+ * @file
+ * Header: Version
+ *
+ * All REvoSim code is released under the GNU General Public License.
+ * See LICENSE.md files in the programme directory.
+ *
+ * All REvoSim code is Copyright 2018 by Mark Sutton, Russell Garwood,
+ * and Alan R.T. Spencer.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or (at
+ * your option) any later version. This program is distributed in the
+ * hope that it will be useful, but WITHOUT ANY WARRANTY.
+ */
+
+#ifndef VERSION_H
+#define VERSION_H
+
+//Software Versions
+#define MAJORVERSION 2
+#define MINORVERSION 0
+#define PATCHVERSION 0
+
+//Save File Version
+#define FILEVERSION 1
+
+//Legal Stuff
+#define COPYRIGHT "Copyright © 2018 Mark Sutton, Russell Garwood, Alan R.T.Spencer"
+#define LICENCE "This program comes with ABSOLUTELY NO WARRANTY. This is free software, and you are welcome to redistribute it under the conditions of the GPL v3  license"
+
+//Programme Name
+#define PRODUCTNAME "REvoSim"
+
+//Github
+//ARTS - This will need to be changed once we settle on a name for the programme
+#define GITHUB_URL "https://github.com/"
+#define GITREPOSITORY "marksutton/EvoSim"
+
+//Readthedocs
+#define READTHEDOCS_URL "https://revosim.readthedocs.io/en/latest/"
+
+#endif // VERSION_H
