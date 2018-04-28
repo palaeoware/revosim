616872f8e05359ca7430d2053110143552e26253
Dr Alan R.T. Spencer <a.r.t.spencer@gmail.com>
2018-04-28 11:05:48 +0100

    Added back in the QT version check for HDPI

--- main.cpp
+++ main.cpp
@@ -38,8 +38,9 @@ int main(int argc, char *argv[])
 
     //This has QPixmap images use the @2x images when available
     //See this bug for more details on how to get this right: https://bugreports.qt.io/browse/QTBUG-44486#comment-327410
-    QApplication::setAttribute(Qt::AA_UseHighDpiPixmaps);
-
+    #if (QT_VERSION >= 0x050600)
+        QApplication::setAttribute(Qt::AA_UseHighDpiPixmaps);
+    #endif
     QApplication application(argc, argv);
 
     //Close on last window close
