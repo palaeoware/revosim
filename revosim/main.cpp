60c9a249490f911073a18e571ecbf13b0081183b
Russell <russell.garwood@manchester.ac.uk>
2019-01-07 11:00:08 +0000

    Updated conditional defines in main.cpp to ensure that revosim compiles with pre qt 5.6

--- revosim/main.cpp
+++ revosim/main.cpp
@@ -33,14 +33,16 @@
  */
 int main(int argc, char *argv[])
 {
+
+#if (QT_VERSION >= 0x050600)
     //This has the app draw at HiDPI scaling on HiDPI displays, usually two pixels for every one logical pixel
     QApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
 
     //This has QPixmap images use the @2x images when available
     //See this bug for more details on how to get this right: https://bugreports.qt.io/browse/QTBUG-44486#comment-327410
-#if (QT_VERSION >= 0x050600)
     QApplication::setAttribute(Qt::AA_UseHighDpiPixmaps);
 #endif
+
     QApplication application(argc, argv);
 
     //Close on last window close
